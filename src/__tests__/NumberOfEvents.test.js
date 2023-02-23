6baf829d77b538fbed82dc150e19b8e21a0dbec3
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-02-23 16:21:02 -0500

    Create NumberOfEvents.test.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsWrapper;
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
+    });
+
+    test('render text input', () => {
+        expect(NumberOfEventsWrapper.find('.number')).toHaveLength(1);
+    });
+
+    test('render text input correctly', () => {
+        const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+        expect(NumberOfEventsWrapper.find('.number').prop('value')).toBe(numberOfEvents);
+    });
+
+    test('change state when text input changes', () => {
+        const eventObject = { target: { value: 32 } };
+        NumberOfEventsWrapper.find('.number').simulate('change', eventObject);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+    });
+});
