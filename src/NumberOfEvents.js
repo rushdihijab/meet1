f0f97bf02eca1691b49f064420c838427f8f431e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-02 16:04:20 -0500

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -10,7 +10,7 @@ class NumberOfEvents extends Component {
         this.setState({
             numberOfEvents: value,
         });
-        this.props.updateEvents(null, value);
+        // this.props.updateEvents(null, value);
     };
 
     render() {
