ee4447aa750ea1df471db9d23ca19fefc24bff9e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-02 16:04:26 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
+import { mockData } from './mock-data';
 //  the extractLocations function extracts and returns all unique locations from an array of event objects.
 
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
     return locations;
+};
+
+export const getEvents = async () => {
+    return mockData;
 };
