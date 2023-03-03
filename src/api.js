b3dd45aca71017f1ac90a9544b73e11c7858c550
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-03 17:21:32 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -12,7 +12,6 @@ export const extractLocations = (events) => {
 export const getAccessToken = async () => {
     const accessToken = localStorage.getItem('access_token');
     const tokenCheck = accessToken && (await checkToken(accessToken));
-
     if (!accessToken || tokenCheck.error) {
         await localStorage.removeItem("access_token");
         const searchParams = new URLSearchParams(window.location.search);
@@ -27,11 +26,10 @@ export const getAccessToken = async () => {
         return code && getToken(code);
     }
     return accessToken;
-};
-
+}
 const checkToken = async (accessToken) => {
     const result = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+        `https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/get-events/{access_token}`
     )
         .then((res) => res.json())
         .catch((error) => error.json());
@@ -52,9 +50,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url =
-            "https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/get-event"
-            + "/" + token;
+        const url = 'https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url' + '/' + token;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
@@ -79,12 +75,11 @@ const removeQuery = () => {
         window.history.pushState("", "", newurl);
     }
 };
-
 const getToken = async (code) => {
     try {
         const encodeCode = encodeURIComponent(code);
 
-        const response = await fetch("https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/token" + "/" + encodeCode);
+        const response = await fetch('https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
@@ -95,3 +90,103 @@ const getToken = async (code) => {
         error.json();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const getAccessToken = async () => {
+//     const accessToken = localStorage.getItem('access_token');
+//     const tokenCheck = accessToken && (await checkToken(accessToken));
+
+//     if (!accessToken || tokenCheck.error) {
+//         await localStorage.removeItem("access_token");
+//         const searchParams = new URLSearchParams(window.location.search);
+//         const code = await searchParams.get("code");
+//         if (!code) {
+//             const results = await axios.get(
+//                 "https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+//             );
+//             const { authUrl } = results.data;
+//             return (window.location.href = authUrl);
+//         }
+//         return code && getToken(code);
+//     }
+//     return accessToken;
+// };
+
+// const checkToken = async (accessToken) => {
+//     const result = await fetch(
+//         `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+//     )
+//         .then((res) => res.json())
+//         .catch((error) => error.json());
+
+//     return result;
+// };
+
+// export const getEvents = async () => {
+//     NProgress.start();
+
+//     if (window.location.href.startsWith("http://localhost")) {
+//         NProgress.done();
+//         return mockData;
+//     }
+
+
+//     const token = await getAccessToken();
+
+//     if (token) {
+//         removeQuery();
+//         const url =
+//             "https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/get-event"
+//             + "/" + token;
+//         const result = await axios.get(url);
+//         if (result.data) {
+//             var locations = extractLocations(result.data.events);
+//             localStorage.setItem("lastEvents", JSON.stringify(result.data));
+//             localStorage.setItem("locations", JSON.stringify(locations));
+//         }
+//         NProgress.done();
+//         return result.data.events;
+//     }
+// };
+
+// const removeQuery = () => {
+//     if (window.history.pushState && window.location.pathname) {
+//         var newurl =
+//             window.location.protocol +
+//             "//" +
+//             window.location.host +
+//             window.location.pathname;
+//         window.history.pushState("", "", newurl);
+//     } else {
+//         newurl = window.location.protocol + "//" + window.location.host;
+//         window.history.pushState("", "", newurl);
+//     }
+// };
+
+// const getToken = async (code) => {
+//     try {
+//         const encodeCode = encodeURIComponent(code);
+
+//         const response = await fetch("https://r14q868nt1.execute-api.us-east-1.amazonaws.com/dev/api/token/" + "/" + encodeCode);
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`)
+//         }
+//         const { access_token } = await response.json();
+//         access_token && localStorage.setItem("access_token", access_token);
+//         return access_token;
+//     } catch (error) {
+//         error.json();
+//     }
+// }
