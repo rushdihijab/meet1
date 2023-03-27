8e87ce972542114eebf02e09df3189955c34eccd
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-27 17:29:01 -0400

    Update CitySearch.js
    add info alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,32 @@
-
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
+        infoText:''
     }
     handleInputChanged = (event) => {
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions,
-        });
-    };
-
+        if (suggestions.length === 0) {
+            this.setState({
+              query: value,
+              infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+          } else {
+            return this.setState({
+              query: value,
+              suggestions,
+              infoText:''
+            });
+          }
+        };
+      
     handleItemClicked = (suggestion) => {
         this.setState({
             query: suggestion,
@@ -38,6 +47,7 @@ class CitySearch extends Component {
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
+                 <InfoAlert text={this.state.infoText} />
 
                 <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
