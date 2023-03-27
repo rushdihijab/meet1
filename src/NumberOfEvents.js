d405c0f5a4442e947f3d9d637f82719bbfc36747
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-27 17:28:41 -0400

    Update NumberOfEvents.js
    add error alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
         numberOfEvents: this.props.numberOfEvents,
+        errorText: '',
     };
 
     handleInputChanged = (event) => {
         const value = event.target.value;
-        this.setState({
-            numberOfEvents: value,
-        });
-
-      this.setState({
-        numberOfEvents: value,
-      });
-      this.props.updateNumEvents(value);
+        if (value < 1) {
+            this.setState({
+                errorText: 'Select number from 1 to 32',
+                numberOfEvents: 1,
+            });
+        } else if (value > 32) {
+            this.setState({
+                errorText: 'Select number from 1 to 32',
+                numberOfEvents: 32,
+            });
+        } else {
+            this.setState({
+                errorText: '',
+                numberOfEvents: value,
+            });
+            this.props.updateNumEvents(value);
+        }
     };
 
     render() {
         return (
             <div className="NumberOfEvents">
-                <p>number of events would you like to see?</p>
+                <p>Number of events would you like to see?</p>
                 <input
                     type="number"
                     className="number"
                     value={this.state.numberOfEvents}
                     onChange={this.handleInputChanged}
                 />
+                <ErrorAlert text={this.state.errorText} />
             </div>
         );
     }
