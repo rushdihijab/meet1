f377fcfa1da5a4142a0a8f0ba5ff8563bdf00cbc
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-29 15:30:22 -0400

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
 import Event from './Event';
-// import { OfflineAlert } from './Alert';
+
 class EventList extends Component {
-    // state = {
-    //     showCacheAlert: true,
-    //   };
     
-    //   hideAlert = () => {
-    //     this.setState({
-    //       showCacheAlert: false,
-    //     });
-    //   };
     
 
     render() {
         const { events } = this.props;
         return (
-        //     <div>
-        // {!navigator.onLine ? (
-        //   <div
-        //     onClick={this.hideAlert}
-        //     style={
-        //       this.state.showCacheAlert
-        //         ? { display: "flex", justifyContent: "center" }
-        //         : { display: "none" }
-        //     }
-        //   >
-        //     <OfflineAlert text="Data is coming from cache, internet connection required for updated events " />
-        //   </div>
-        // ) : (
-        //   <></>
-        // )}
+        
             <ul className="EventList">
                 {events.map(event =>
                     <li key={event.id}>
@@ -38,7 +16,7 @@ class EventList extends Component {
                     </li>
                 )}
             </ul>
-            // </div>
+        
         );
     }
 }
