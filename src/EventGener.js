1176fd51fed1221383a2024e56835c707e9d0acc
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-31 15:32:51 -0400

    Update EventGener.js

--- src/EventGener.js
+++ src/EventGener.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
 
 
 
@@ -38,7 +38,7 @@ const EventGenre = ({ events }) => {
                         <Cell key={`cell-${index}`} fill={colors[index]} />
                     ))}
                 </Pie>
-                  <Legend verticalAlign="bottom" height={50}/>
+                 
             </PieChart>
         </ResponsiveContainer>
     )
