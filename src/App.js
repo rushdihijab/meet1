c6b08f6aa87900f01e10b25dbf1ef98e72b93d4e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-03-31 14:55:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,20 +21,24 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
+
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-    true;
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    // if ((code || isTokenValid) && this.mounted) {
-    getEvents().then((events) => {
-    if (this.mounted) {
-    this.setState({ events, locations: extractLocations(events) });
+    const code = searchParams.get('code');
+    const isLocal = window.location.href.startsWith('http://localhost')
+      ? true
+      : code || isTokenValid;
+    this.setState({ showWelcomeScreen: !isLocal });
+    if (isLocal && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
     }
-    });
-    // }
-    }
+  }
+
   componentWillUnmount() {
     this.mounted = false;
   }
